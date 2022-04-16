02c9efb97dbf0273c67d5cb66e70f3a14c5a5650
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2022-04-16 16:08:22 -0300

    possibilita escolher enviar mensagens publicas ou privadas

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
-// Pegar paricipantes
-
-
 let user;
+let lastMessage;
+let adresssee = "Todos";
+let visibility = "Público";
+let visibilityText = "publicamente"
 
-// mensagens
+function showSideBar() {
+    document.querySelector(".dark-background").classList.remove("removed");
+    document.querySelector(".side-bar").classList.add("show-screen"); // alterar nome
+}
+
+function hideSideBar() {
+    document.querySelector(".dark-background").classList.add("removed");
+    document.querySelector(".side-bar").classList.remove("show-screen"); // alterar nome
+
+}
+
 
 function addEntraceStatus(message) {
 
@@ -34,7 +45,7 @@ function addConversationMessage(message, messageType) {
 }
 
 function verifyReservedAdressee(message) {
-    if(message.to === user) {
+    if(message.to === user.name || message.from === user.name) {
         return true;
     }
 
@@ -45,9 +56,9 @@ function sendMessage(btn) {
 
     const message = {
         from: user.name,
-        to: "Todos",
+        to: adresssee,
         text: btn.parentNode.querySelector("input").value,
-        type: "message"
+        type: visibility === "Público" ? "message" : "private_message",
     }
 
     btn.parentNode.querySelector("input").value = "";
@@ -63,20 +74,42 @@ function sendMessage(btn) {
 function setMessageStatus(response) {
     const messages = response.data;
     let messageType = "";
+
+    let indexLastMessage;
+    let lastMessageHour;
     
-    for(let i = 0; i < messages.length; i++) {
-        switch(messages[i].type) {
+    if(lastMessage !== undefined) {
+        lastMessageHour = messages.find(message => message.time === lastMessage.time);
+        indexLastMessage = messages.indexOf(lastMessageHour) + 1;
+        
+    } else {
+        indexLastMessage = 0;
+        
+    }
+
+    let filterMessages = messages.slice(indexLastMessage, messages.length);
+    
+    for(let i = 0; i < filterMessages.length; i++) {
+
+        if(i === filterMessages.length - 1) {
+            lastMessage = filterMessages[i];
+        }
+
+        switch(filterMessages[i].type) {
             case "status":
-                addEntraceStatus(messages[i], messageType);
+                addEntraceStatus(filterMessages[i], messageType);
                 break;
             case "message":
                 messageType = "";
-                addConversationMessage(messages[i], messageType);
+                addConversationMessage(filterMessages[i], messageType);
                 break;
                 case "private_message":
                     messageType = "reserved";
-                if(verifyReservedAdressee(messages[i])) {
-                    addConversationMessage(messages[i], messageType)
+                    console.log(verifyReservedAdressee(filterMessages[i]));
+                if(verifyReservedAdressee(filterMessages[i])) {
+                    console.log("olá");
+                    addConversationMessage(filterMessages[i], messageType)
+                    console.log(filterMessages[i]);
                 }
                 break;
             default:
@@ -102,19 +135,68 @@ function keepConected() {
     });
 }
 
-function verifyOnlineParticipants() {
-    const promiseParticipants = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants");
+function listOnlineParticipants(participants) {
+    const listParticipants = document.querySelector(".participants-list");
 
-    promiseParticipants.then(function(response) {
-        console.log(response.data);
-    });
+    listParticipants.innerHTML = `
+        <div class="participant" onclick="selectAdressee(this)">
+            <div>
+                <ion-icon name="people"></ion-icon>
+                <p>Todos</p>
+            </div>
+            <ion-icon name="checkmark-sharp" class="check-icon visible"></ion-icon>
+        </div>
+        `;
+        
+    for(let i = 0; i < participants.length; i++) {
+        listParticipants.innerHTML += `
+            <div class="participant" onclick="selectAdressee(this)">
+                <div>
+                    <ion-icon name="people"></ion-icon>
+                    <p>${participants[i].name}</p>
+                </div>
+                <ion-icon name="checkmark-sharp" class="check-icon"></ion-icon>
+            </div>
+        `;
+    }
 
 }
 
+function verifyOnlineParticipants() {
+    const promiseParticipants = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants");
+
+    promiseParticipants.then(function (response) {
+        const participants = response.data;
+        listOnlineParticipants(participants);
+    });
+}
+
+function selectAdressee(participantContainer) {
+    document.querySelector(".participants-list .check-icon.visible").classList.remove("visible");
+    participantContainer.querySelector(".check-icon").classList.add("visible");
+
+    adresssee = participantContainer.querySelector("p").innerHTML;
+
+    document.querySelector(".bottom-bar p").innerHTML = `Enviando para ${adresssee} (${visibilityText})`;
+}
+
+function selectVisibility(visibilityContainer) {
+    document.querySelector(".visibility-option .check-icon.visible").classList.remove("visible");
+    visibilityContainer.querySelector(".check-icon").classList.add("visible");
+    
+    visibility = visibilityContainer.querySelector("p").innerHTML;
+    visibilityText = visibility === "Público" ? "publicamente" : "reservadamente";
+
+    document.querySelector(".bottom-bar p").innerHTML = `Enviando para ${adresssee} (${visibilityText})`;
+}
+
 function initializeSite() {
-    setInterval(loadMessages, 3000);
+    setInterval(loadMessages, 10000);
     setInterval(keepConected, 4000);
-    //setInterval(verifyOnlineParticipants, 5000);
+    setInterval(verifyOnlineParticipants, 10000);
+
+    loadMessages();
+    verifyOnlineParticipants();
 }
 
 
